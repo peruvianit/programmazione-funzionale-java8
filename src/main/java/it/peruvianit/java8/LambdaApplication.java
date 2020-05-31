3ad1c0541b12c017fded13487d4b1677b30ebea4
Sergio Alexander Arellano Diaz <sergioarellanodiaz@hotmail.com>
2020-05-31 18:06:36 +0200

    Collezioni Lambda : lista ordinata

--- src/main/java/it/peruvianit/java8/LambdaApplication.java
+++ src/main/java/it/peruvianit/java8/LambdaApplication.java
@@ -1,9 +1,13 @@
 package it.peruvianit.java8;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import it.peruvianit.java8.core.service.CollectionService;
 import it.peruvianit.java8.core.service.LambdaService;
 
 @SpringBootApplication
@@ -17,6 +21,22 @@ public class LambdaApplication {
 		
 		System.out.println(lambdaService.helloLambda("helloLambda"));
 		
+		
+		// Collezioni
+		
+		List<String> list = new ArrayList<>();
+		list.add("Pera");
+		list.add("Mela");
+		list.add("Arancia");
+		list.add("Kiwi");
+		
+		
+		CollectionService collectionService = applicationContext.getBean(CollectionService.class);
+		
+		// Ordena lista
+		collectionService.ordena(list);
+		
+		list.forEach((String ele) -> System.out.println(ele));
 	}
 
 }
