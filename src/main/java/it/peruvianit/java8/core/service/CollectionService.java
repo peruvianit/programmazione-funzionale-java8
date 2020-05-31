e6015abcd6c54684bad9cdfdd65fb6e72cdc1a65
Sergio Alexander Arellano Diaz <sergioarellanodiaz@hotmail.com>
2020-05-31 18:06:36 +0200

    Collezioni Lambda : Ordine lista

---
+++ src/main/java/it/peruvianit/java8/core/service/CollectionService.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package it.peruvianit.java8.core.service;
+
+import java.util.List;
+
+/**
+ * @author Sergio Arellano {PeruViANit}
+ *
+ * @version 1.0.0
+ * @since 1.0.1
+ */
+public interface CollectionService {
+
+	/**
+	 * ordena una lista utlizzando espresioni Lambda
+	 * 
+	 * @param list
+	 * 
+	 * @return
+	 * 
+	 * @version 1.0.0
+	 * @since 1.0.1
+	 */
+	void ordena(List<String> list);
+	
+	String info();
+}
---
+++ src/main/java/it/peruvianit/java8/core/service/impl/CollectionServiceImpl.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package it.peruvianit.java8.core.service.impl;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import it.peruvianit.java8.core.service.CollectionService;
+
+/**
+ * <pre>
+ * 	Utilizzo delle collezione nella progrmazione funzionale
+ * </pre>
+ * 
+ * @author Sergio Arellano {PeruViANit}
+ *
+ */
+@Service
+public class CollectionServiceImpl implements  CollectionService {
+
+	@Override
+	public String info() {
+		return "Classe per il utilizzo delle prove per la programazione funzionale";
+		
+	}
+
+	@Override
+	public void ordena(List<String> list) {
+		
+		list.sort((String p1, String p2) -> p1.compareTo(p2));;
+	}
+
+}
